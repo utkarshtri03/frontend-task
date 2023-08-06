a10ae85de5b35f8af51b41d8d586c3221637ac5e
utkarshtri03 <utkarshtri03@gmail.com>
2023-08-06 10:16:08 +0530

    design

--- app/QueriesHistory.js
+++ app/QueriesHistory.js
@@ -1,12 +1,11 @@
-'use client'
-import React from 'react'
-import Searchbar from './Searchbar'
-import { useQueryContext } from './Context';
-import { useState } from 'react';
+"use client";
+import React from "react";
+import { useQueryContext } from "./Context";
+import { useState } from "react";
 
 const QueriesHistory = () => {
-  const {queriesHistory,setSelectedQuery} = useQueryContext();
-  const [searchTerm, setSearchTerm] = useState('');
+  const { queriesHistory, setSelectedQuery } = useQueryContext();
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSearchInputChange = (event) => {
     setSearchTerm(event.target.value);
@@ -16,22 +15,24 @@ const QueriesHistory = () => {
     setSelectedQuery(query);
   };
 
-  // Filter the queriesAvailable based on the searchTerm
   const filteredQueries = queriesHistory.filter((query) =>
     query.toLowerCase().includes(searchTerm.toLowerCase())
   );
   return (
-    <div className='h-1/2 bg-slate-700'>
-        <p>Queries History</p>
-        <input placeholder='Search....' value={searchTerm} onChange={handleSearchInputChange}/>
-        <ul>
-          {filteredQueries.map((val) => {
-            return <li onClick={() => handleQueryClick(val)}>{val}</li>;
-          })}
-        </ul>
-        
+    <div className="h-1/2 bg-slate-700">
+      <p>Queries History</p>
+      <input
+        placeholder="Search...."
+        value={searchTerm}
+        onChange={handleSearchInputChange}
+      />
+      <ul>
+        {filteredQueries.map((val) => {
+          return <li onClick={() => handleQueryClick(val)}>{val}</li>;
+        })}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default QueriesHistory
+export default QueriesHistory;
